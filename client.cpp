2d6e8da70abadd1a6dce3a912484fb19cc74a47d
Sidharth <sidharth.edupuganti@gmail.com>
2021-12-09 23:52:06 -0600

    sick

--- client.cpp
+++ client.cpp
@@ -8,6 +8,23 @@
 
 using namespace std;
 
+int convert(char num[]) {
+   int len = strlen(num);
+   int base = 1;
+   int temp = 0;
+   for (int i=len-1; i>=0; i--) {
+      if (num[i]>='0' && num[i]<='9') {
+         temp += (num[i] - 48)*base;
+         base = base * 16;
+      }
+      else if (num[i]>='A' && num[i]<='F') {
+         temp += (num[i] - 55)*base;
+         base = base*16;
+      }
+   }
+   return temp;
+}
+
 void handleStatusCode(string newURL, string keepFilename) {
 
     string URL = newURL;
@@ -182,6 +199,18 @@ int main(int argc, char** argv) {
     char msg[256];
     msg[255] = '\0';
 
+    // char msgTest[15000];
+    // msgTest[14999] = '\0';
+    // int nbytes2 = chan->cread(&msgTest, 14999);
+
+    // for (int i = 0; i < nbytes2; i++) {
+    //     cout << msgTest[i];
+    // }
+
+    // cout << endl;
+
+    
+
     ofstream output;
 
     int nbytes;
@@ -189,10 +218,42 @@ int main(int argc, char** argv) {
     bool foundStatusCode = false;
     bool isChunked = false;
 
+    int i = 0;
+
+    int totalNum = 0;
+    int value = 0;
+
 
     while (true) {
 
+        // cout << endl;
+
+
+
         nbytes = chan->cread(&msg, 255);
+
+        // cout << "HELLO" << endl;
+
+        // for (int i = 0; i < nbytes; i++) {
+        //     cout << msg[i];
+        // }
+
+        // cout << endl;
+        // break;
+
+        // if (i == 11) {
+        //     // cout << "Char is: " << msg[57] << endl;
+        //     // cout << "Char is: " << msg[58] << endl;
+        //     // cout << "Char is: " << msg[59] << endl;
+        //     // cout << "Char is: " << msg[60] << endl;
+        //     // cout << "Char is: " << msg[61] << endl;
+        //     break;
+        // }
+
+        // if (nbytes < 255) {
+        //     string firstline(msg);
+        //     cout << firstline << endl;
+        // }
 
         if (nbytes == 0) {
             break;
@@ -219,7 +280,7 @@ int main(int argc, char** argv) {
                 }
 
                 handleStatusCode(url, filename);
-                break;
+                // break;
             } else if (statusCode == '4') {
                 cout << "4XX STATUS CODE" << endl;
                 exit(1);
@@ -229,15 +290,100 @@ int main(int argc, char** argv) {
  
         }
 
+        int hexadecimal;
+        int checkNBytes = 0;
+        // int total;
+
+
+
+        for (int i = 0; i < nbytes; i++) {
+            // if (msg[i] == '\r' && msg[i+1] == '\n' && msg[i+6] == '\r' && msg[i+7] == '\n') {
+            //         cout << endl;
+            //         cout << msg[i + 2];
+            //         cout << msg[i + 3];
+            //         cout << msg[i + 4];
+            //         cout << msg[i + 5];
+
+            //         cout << endl;
+
+            //         string charStr;
+
+            //         charStr.push_back(msg[i + 2]);
+            //         charStr.push_back(msg[i + 3]);
+            //         charStr.push_back(msg[i + 4]);
+            //         charStr.push_back(msg[i + 5]);
+
+            //         int stringLen = charStr.length();
+
+            //         char hexa2Int[stringLen + 1];
+
+            //         strcpy(hexa2Int, charStr.c_str());
+
+            //         hexadecimal = convert(hexa2Int);
+
+            //         cout << "Conversion to Int: " << hexadecimal << endl;
+            //         total+=hexadecimal;
+            // }
+
+            if (msg[i] == '\r' && msg[i + 1] == '\n' && msg[i+2] == '\r' && msg[i+3] == '\n' && msg[i+8] == '\r') {
+                string charStr;
+                cout << "TEST: ";
+                cout << msg[i + 4];
+                cout << msg[i + 5];
+                cout << msg[i + 6];
+                cout << msg[i + 7];
+                cout << endl;
+
+                charStr.push_back(msg[i + 4]);
+                charStr.push_back(msg[i + 5]);
+                charStr.push_back(msg[i + 6]);
+                charStr.push_back(msg[i + 7]);
+
+                cout << charStr << endl;
+
+                int stringLen = charStr.length();
+
+                char str2Hexa[stringLen + 1];
+
+                strcpy(str2Hexa, charStr.c_str());
+
+                hexadecimal = convert(str2Hexa);
+                value = hexadecimal;
+                cout << "value: " << value << endl;
+
+
+                // cout << "hexadecimal form of first size: " << hexadecimal << endl;
+                break;
+                // break;
+            }
+
+
+
+            
+
+
+
+            // if (msg[i] == '\n') {
+            //     cout << endl;
+            //     cout << "backslashN Yes" << endl;
+            //     cout << "Char - 1: " << msg[i - 1] << endl;
+            //     cout << "Char + 1: " << msg[i + 1] << endl;
+            //     cout << endl;
+            // }
+            
+        }
+
+        // checkNBytes+=nbytes;
+
         if (!isChunked) {
             // chan->cread(&msg, 255);
 
             string firstLine(msg);
             
-            cout << endl;
-            cout << firstLine << endl;
-            cout << endl;
-
+            // cout << endl;
+            // cout << firstLine << endl;
+            // cout << endl;
+            // break;
 
             stringstream ss(firstLine);
             string line;
@@ -247,37 +393,55 @@ int main(int argc, char** argv) {
                     // cout << line.find("transfer-encoding: chunked") << endl;
                 }
             }
-            
 
         }
 
 
+
+
         if (isChunked) {
 
-            // string tempString(msg);
-            // cout << tempString[10] << endl;
-            // cout << tempString[11] << endl;
-            // cout << tempString[12] << endl;
-            // cout << tempString.find("transfer-encoding: chunked") << endl;
-            // cout << tempString[tempString.find("transfer-encoding: chunked") + 36] << endl;
+            cout << "nbytes: " << nbytes << endl;
+
+    
+            if (begin) {
+                cout << "value: " << value << endl;
+
+                value-=nbytes;
+
+                if (value <= 0) {
+                    break;
+                }
+
+
+            
+            }
+
+
 
             string s;
-            if (nbytes < 255) {
-                s = "";
-                for (int i = 0; i < nbytes; i++) {
-                    s+= msg;
-                }
-            } else {
+            // if (nbytes < 255) {
+            //     s = "";
+            //     for (int i = 0; i < nbytes; i++) {
+            //         s+= msg;
+            //     }
+            // } else {
                 s = string(msg);
                 if (s.find("transfer-encoding: chunked") != string::npos) {
                     if (!begin) {
                         int index = s.find("transfer-encoding: chunked") + 36;
+           
+                        int strLength = s.size();
                         s = s.substr(index);
+                        
+                        value-=(strLength - index);
+                        cout << "value: " << value << endl;
+                        // cout << s << endl;
+                        // cout << "index: " << index << endl;
                         begin = true;
                     }
                 }
-            }
-
+            // }
 
             if (begin) {
                 output.open(filename, std::ios::app | std::ios::binary);
@@ -311,11 +475,15 @@ int main(int argc, char** argv) {
                 }
             }
 
+
             if (begin) {
                 output.open(filename, std::ios::app | std::ios::binary);
                 output << s;
                 output.close();
             }
         }
+        i++;
     }
+
+    cout << "total characters: " << totalNum << "ok" << endl;
 }
